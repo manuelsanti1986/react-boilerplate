4ec9cbe497ebddc24993b5e6e026839d2a2d8a5d
Manuel Santiago <msanti16@jhu.edu>
2021-05-08 15:56:15 -0400

    Added web-vitals

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
 import ComponentOne from './components/ComponentOne';
 import './styles/styles.scss';
 
-ReactDOM.render(<ComponentOne />, document.getElementById("app"));
+ReactDOM.render(
+    <React.StrictMode>
+        <ComponentOne />
+    </React.StrictMode>,
+    document.getElementById("app"));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
