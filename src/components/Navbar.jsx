c3390f0ed4256e89306bc365cc8755c697b0346a
Manuel Santiago <msanti16@jhu.edu>
2021-05-08 18:36:16 -0400

    Cleaned up code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,13 @@ import {NavLink} from "react-router-dom";
 const Navbar = () => (
     <div className={"tab"}>
         <NavLink className="tablinks" to="/one">
-            <i className={"fa fa-user-o"} />
             One
         </NavLink>
         <td/>
         <NavLink className="tablinks" to="/two">
-            <i className={"fa fa-gear"} />
             Two
         </NavLink>
         <NavLink className="tablinks" to="/three">
-            <i className={"fa fa-sign-out"} />
             Three
         </NavLink>
     </div>
