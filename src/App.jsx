3057a1f9b2a2d8c30e019690d942ecdab6c3a4c4
Manuel Santiago <msanti16@jhu.edu>
2021-05-08 15:05:43 -0400

    Router is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
-
 import RoutingComponent from './RoutingComponent';
 
 export default class App extends Component {
